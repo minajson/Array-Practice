8e984d264aff4310bdfa73594993356566a2bf8f
minajson <minajjumbo@gmail.com>
2025-07-25 07:24:46 +0100

    added loop divisible by 3

--- loop.js
+++ loop.js
@@ -15,4 +15,14 @@ for(let i = 1; i <=100; i++) {
         oddsum+= i;
     }
 }
-  
+    console.log("Sum of odd numbers 1-100:", sum);
+
+// Count numbers between 1 and 100 divisible by 3
+let count = 0;
+for (let i = 1; 1 <= 100; i++) {
+    if (i % 3 === 0) {
+        count ++;
+    }
+}
+console.log("Numbers divisible by 3:", count);
+
