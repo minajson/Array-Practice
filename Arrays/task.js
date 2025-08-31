8487c0a49c25adb1ad3a3673164305e70c2425ee
minajson <minajjumbo@gmail.com>
2025-08-31 07:10:59 +0100

    solving codingjs problems warmup-1 number 4- diff21

--- Arrays/task.js
+++ Arrays/task.js
@@ -3,3 +3,13 @@ function firstLast6 (nums){
     return nums[0] === 6 || nums[nums.length -1] === 6;
 }
 console.log(firstLast6(nums));
+
+function diff21(n) {
+    if (n <= 21) {
+        return 21 - n; 
+        } else {
+       return (n -21) * 2;
+        }
+
+        }
+   
